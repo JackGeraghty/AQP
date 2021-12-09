3cd7b264ea56f2890c852eb5091711cba601a5eb
Jack Geraghty <jack.geraghty@ucdconnect.ie>
2021-12-09 13:01:52 +0000

    run_exmaple.sh and prepend_files.py changes
    Modified the prepend_files.py script to not be a pain to use. Previously if it was run more than once/on a file that dataframe that already had the desired prefix it would still prefix each cell
    
    Example
    -------
    resources/myfile.mp4 -> resources/resources/myfile.mp4
    
    or the more generic
    
    {args.prepend_with}/myfile.mp4 -> {args.prepend_with}/{args.prepend_with}/myfile.mp4
    
    The script has now been changed so that it only prefixes cells that don't already start with the desired prefix
    
    Another small modification involves being able to pass through the names of the reference and test columns so that it's not hardcoded to "Ref_Wave" and "Test_Wave"
    prepend_files.py has also been moved to the scripts directory
    These changes should make the scipt more usable outside of the example scripts
    
    run_example.sh has also been updated. It now checks the return code of the prepend_files.py script and will only run the pipeline if the exit code is 0. Should save some precious seconds :]

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 sh get_quickstart_genspeech.sh 
-cd ..
-python3  utils/prepend_files.py --prepend_with "resources/" --dataset "resources/quickstart_genspeech.csv"
-python3  pipeline.py --root_node_id "Load DF" --graph_config_path "config/example.json"
+python prepend_files.py --prepend_with "resources/" --dataset "../resources/quickstart_genspeech.csv"
+if [ $? -eq 0 ]
+then
+    cd ..
+    python pipeline.py --root_node_id "Load DF" --graph_config_path "config/example.json"
+else
+    echo "Prepend files exitted with error code 1, not running pipeline"
+fi
